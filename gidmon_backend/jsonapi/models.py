d4ea445cf6d14ac514ccc88423b2084842fe484a
noctoz <noctoz@outlook.com>
2016-12-07 02:14:22 +0100

    Fix Beer model admin view string

--- gidmon_backend/jsonapi/models.py
+++ gidmon_backend/jsonapi/models.py
@@ -4,8 +4,14 @@ class Beer(models.Model):
     name = models.CharField(max_length=100)
     abv = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.name
+
     class JSONAPIMeta:
         resource_name = "beers"
 
 class Recipe(models.Model):
     beer = models.OneToOneField(Beer)
+
+    def __str__(self):
+        return 'Recipe: %s' % self.beer.name
